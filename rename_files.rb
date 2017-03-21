7e09e80d313c6f440cf21e8fa9cec7ccd9167c1c
kspurgin <kspurgin@email.unc.edu>
2017-03-21 16:54:29 -0400

    Add helper script

---
+++ rename_files.rb
@@ -0,0 +1,22 @@
+Dir.chdir('incoming_marc')
+
+Dir.glob("*.{mrc,mrk,zip}").each { |filename|
+  pieces = filename.split('.')
+  pieces.shift
+  pieces.shift
+  updatetype = pieces.shift
+  filetype = pieces.pop
+  filedate = pieces.shift
+  filetime = pieces.shift
+  if pieces.size == 2
+    segment = pieces.shift
+  else
+    segment = '_'
+  end
+  order = pieces.shift
+  newname = "#{segment}_#{filedate}_#{updatetype}_#{order}.#{filetype}"
+  File.rename(filename, newname)
+
+}
+
+
