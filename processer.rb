f4482913c6dae1394e19ba98925751857cf9d77e
kspurgin <kspurgin@email.unc.edu>
2017-03-23 17:31:07 -0400

    Add variable fields from spec with replacement(s)

--- processer.rb
+++ processer.rb
@@ -2,6 +2,7 @@ $LOAD_PATH << '.'
 require 'yaml'
 require 'marc'
 require 'marc_record'
+require 'pp'
 
 # Get a hash of your config
 config = begin
@@ -14,6 +15,9 @@ config = begin
 in_dir = 'incoming_marc'
 ex_dir = 'last_processed_marc_ORIG'
 out_dir = 'output'
+
+# Set up MARC writers
+out_mrc = MARC::Writer.new("#{out_dir}/output.mrc")
 
 # Find out what workflow we're dealing with and set workflow config
 puts "\n\nEnter file segment/workflow you are processing (SPR2017, AMS, etc.):"
@@ -28,7 +32,7 @@ until config['workflows'].has_key?(segment)
 end
 wconfig = config['workflows'][segment]
 iconfig = config['institution']
-@idtag = iconfig['record id']['tag']
+idtag = iconfig['record id']['tag']
 
 # Pull in our incoming and previously loaded MARC records
 def get_recs(dir)
@@ -67,36 +71,36 @@ end
 #  until we output the processed records.
 
 # Clean IDs in if config says.
-# Set up hash of existing records, keyed by @idtag value, for comparing sets
+# Set up hash of existing records, keyed by idtag value, for comparing sets
 # rec.overlay_point of {'019'=>x} here means:
-#  - this record will be overlaid by incoming record with @idtag value x
-#  - incoming record's @idtag value x presumably does NOT match this record's
-#  -   @idtag value
+#  - this record will be overlaid by incoming record with idtag value x
+#  - incoming record's idtag value x presumably does NOT match this record's
+#  -   idtag value
 #  - the overlay will be on an 019$a value in the incoming record
-@ex_ids = {}
+ex_ids = {}
 
 ex_mrc.each { |rec|
-  clean_id(rec, @idtag) if iconfig['clean ids']
-  @ex_ids[rec[@idtag].value] = rec
+  clean_id(rec, idtag) if iconfig['clean ids']
+  ex_ids[rec[idtag].value] = rec
 }
 
-def get_019_matches(rec)
+def get_019_matches(rec, the_idtag, ex_id_list)
   my019s = rec.get_019_vals
   if my019s.size > 0
     match019 = []
     my019s.each do |chkid|
-      if @ex_ids.has_key?(chkid)
+      if ex_id_list.has_key?(chkid)
         match019 << chkid
         rec.overlay_point << {'019' => chkid}
-        @ex_ids[chkid].overlay_point << {'019' => rec[@idtag].value}
+        ex_id_list[chkid].overlay_point << {'019' => rec[the_idtag].value}
 
         # Interpreting 019-related .overlay_point values
         # {'019'=>'x'}
         # INCOMING RECORD
-        # x = the @recid value in the existing record that will get overlaid
-        #     this incoming rec's 019$a value that matches the @recid value in existing record
+        # x = the recid value in the existing record that will get overlaid
+        #     this incoming rec's 019$a value that matches the recid value in existing record
         # EXISTING RECORD
-        # x = the @recid value of the record that will overlay this record
+        # x = the recid value of the record that will overlay this record
       end
     end
   end
@@ -117,30 +121,69 @@ def put_matching_019_sf_first(rec)
   end
   rec.append(newfield)
 end
-    
+
+def add_marc_var_fields_replacing_values(rec, fspec, replaces)
+  fspec.each { |fs|
+    sfval = ''
+    f = MARC::DataField.new(fs['tag'], fs['i1'], fs['i2'])
+    fs['subfields'].each { |sfs|
+      sfval = sfs[1].dup
+      if replaces.size > 0
+        replaces.each { |findrep|
+          findrep.each_pair { |fnd, rep|
+            sfval.gsub!(fnd, rep)
+          }
+        }
+      end
+      sf = MARC::Subfield.new(sfs[0], sfval)
+      f.append(sf)
+    }
+    rec.append(f)
+  }
+end
+
 
 # Clean IDs in if config says.
 in_mrc.each { |rec|
-  clean_id(rec, @idtag) if iconfig['clean ids']
-  # Set record.overlay_point of incoming record to @idtag info if there's a match on main record id
+  clean_id(rec, idtag) if iconfig['clean ids']
+  # Set record.overlay_point of incoming record to idtag info if there's a match on main record id
   # Since this match relies on main record id being the same in incoming and existing
   #  records, also set record.overlay_point of existing record.
-  if @ex_ids.has_key?(rec[@idtag].value)
-    op = {@idtag => rec[@idtag].value}
+  if ex_ids.has_key?(rec[idtag].value)
+    op = {idtag => rec[idtag].value}
     rec.overlay_point << op
-    exrec = @ex_ids[rec[@idtag].value]
+    exrec = ex_ids[rec[idtag].value]
     exrec.overlay_point << op
   end
 
   if iconfig['overlay matchpoint includes 019']
     # Check for overlays between existing 001 and any 019$a in an incoming record
-    get_019_matches(rec)
+    get_019_matches(rec, idtag, ex_ids)
   end
 
   if iconfig['manipulate 019 for overlay']
     rec.overlay_point.each { |op| put_matching_019_sf_first(rec) if op.has_key?('019') }
   end
+
+  if iconfig['flag overlay type']
+    if rec.overlay_point.size > 0
+      ovtypes = []
+      rec.overlay_point.each { |op| ovtypes << op.keys }
+      ovtypes.flatten!
+      ovtypes_x = []
+      ovtypes.each { |type| ovtypes_x << "OVERLAY on #{type}"}
+      ovtype = ovtypes_x.flatten.join(', ')
+    else
+      ovtype = 'NEW'
+    end
+    add_marc_var_fields_replacing_values(rec, iconfig['overlay type flag spec'], [{'[OVTYPE]'=>ovtype}])
+  end
+
+
+  out_mrc.write(rec)
 }
+  pp(iconfig['overlay type flag spec'])
+
 
 
 
